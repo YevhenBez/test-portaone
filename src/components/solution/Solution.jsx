44400febe5ca34253aff660ba61a9f4977ee209a
YevhenBez <byxgalter@bigmir.net>
2024-03-04 09:57:29 +0200

    Сделал таблицу для данного тестового и нашёл максимальное число согласно задания тестового.  Тестовое для  Education Center Portaone
    Сделал таблицу для данного тестового и нашёл максимальное число согласно задания тестового. Для этого в файле  Solution.jsx   я импортнул массив чисел (в котором я и буду находить это максимальное число)  и также импортнул стили. Затем я создал компонент   Solution   в котором в переменной   maxNumber   я нашёл максимальное число. Обрати внимание, что в переменной   maxNumber   я использовал метод reduce()   Дело в том, что этот метод применяет функцию к каждому элементу массива по очереди, что позволяет обрабатывать большие массивы без переполнения стека вызовов (в противном случае происходит переполнение стека вызовов и просто ничего не разрендерится). Функция reduce() применяется к каждому числу в массиве по очереди, и на каждом шаге она сравнивает текущее максимальное число (max) с текущим числом в массиве (current), и возвращает большее из двух чисел. Значение -Infinity используется как начальное значение для max, чтобы убедиться, что любое число в массиве будет больше этого начального значения. Также в файле  Solution.jsx   я вернул разметку таблицы   и экспортнул компонент   Solution    Затем в файле   index.js   сделал реэкспорт компонента  Solution    (чтобы уменьшить пути к нему при запросе из других файлов). В файле   solution.module.css
      написал стили для таблицы компонента  Solution    Затем в файле   App.jsx   импортнул  компонент  Solution   и разрендерил его. Тестовое для  Education Center Portaone

---
+++ src/components/solution/Solution.jsx
@@ -0,0 +1,61 @@
+import arrayNumbers from '../../path/to/10m.json';
+import css from '../solution/css/solution.module.css';
+
+const Solution = () => {
+  const maxNumber = arrayNumbers.reduce(
+    (max, current) => Math.max(max, current),
+    -Infinity
+  );
+
+  return (
+    <div className={css.solutionContainer}>
+      <table className={css.solutionContainer__table}>
+        <thead>
+          <tr className={css.solutionContainer__table__tr}>
+            <th className={css.solutionContainer__table__tr__th}>
+              максимальне число
+            </th>
+            <th className={css.solutionContainer__table__tr__th}>
+              мінімальне число
+            </th>
+            <th className={css.solutionContainer__table__tr__th}>медіана</th>
+            <th className={css.solutionContainer__table__tr__th}>
+              середнє арифметичне значення
+            </th>
+            <th className={css.solutionContainer__table__tr__th}>
+              найбільша послідовність чисел (які ідуть один за одним), яка
+              збільшується
+            </th>
+            <th className={css.solutionContainer__table__tr__th}>
+              найбільша послідовність чисел (які ідуть один за одним), яка
+              зменьшується
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td className={css.solutionContainer__table__td}>{maxNumber}</td>
+            <td className={css.solutionContainer__table__td}>
+              мінімальне число
+            </td>
+            <td className={css.solutionContainer__table__td}>медіана</td>
+            <td className={css.solutionContainer__table__td}>
+              середнє арифметичне значення
+            </td>
+            <td className={css.solutionContainer__table__td}>
+              найбільша послідовність чисел (які ідуть один за одним), яка
+              збільшується
+            </td>
+            <td className={css.solutionContainer__table__td}>
+              найбільша послідовність чисел (які ідуть один за одним), яка
+              зменьшується
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Solution;
